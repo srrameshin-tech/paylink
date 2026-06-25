39fa315f3c9a59b504fbc00e636529421bf9e990
srrameshin-tech <srrameshin@gmail.com>
2026-06-25 14:17:27 +0530

    Add sw.js - initial PayLink app build

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "paylink-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/app.js",
+  "/qrcode.min.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// network-first strategy
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
